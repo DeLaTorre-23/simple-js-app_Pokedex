7166cde713a2d91524395ef7ea2aaca54a922284
Serch <ztorre23@gmail.com>
2020-09-11 14:30:41 +0200

    Built array with the first 9 pokemons. Each object contain: name, height & types

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let pokemonList = [{
+  name: 'Bulbasaur',
+  height: 0.7,
+  types: ['grass', 'poison']
+  },
+  {
+  name: 'Ivysaur',
+  height: 1,
+  types: ['grass', 'poison']
+  },
+  {
+  name: 'Venusaur',
+  height: 2,
+  types: ['grass', 'poison']
+  },
+  {
+  name: 'Charmander',
+  height: 0.6,
+  types: ['fire']
+  },
+  {
+  name: 'Charmeleon',
+  height: 1.1,
+  types: ['fire']
+  },
+  {
+  name: 'Charizard',
+  height: 1.7,
+  types: ['fire', 'flying']
+  },
+  {
+  name: 'Squirtle',
+  height: 0.5,
+  types: ['water']
+  },
+  {
+  name: 'Wartortle',
+  height: 1,
+  types: ['water']
+  },
+  {
+  name: 'Blastoise',
+  height: 1.6,
+  types: ['water']
+  }
+];
