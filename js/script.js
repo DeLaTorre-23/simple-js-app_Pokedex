c866395408c772f53b8ab658d6357661b985c26d
Serch <ztorre23@gmail.com>
2020-09-17 19:08:20 +0200

    Creating the List elements and the button for them

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // IIFE protecting the repository array of pokemon
 let pokemonRepository = (function () {
-  let pokemonList = [
+  let repository = [
     {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
     {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
     {name: 'Venusaur', height: 2, types: ['grass', 'poison']},
@@ -20,22 +20,79 @@ let pokemonRepository = (function () {
       'height' in pokemon &&
       'types' in pokemon
     ) {
-      pokemonList.push(pokemon);
+      repository.push(pokemon);
     } else {
-      document.write('This pokemon isn\'t correct');
+      document.write('This pokemon is not correct');
     }
   }
 
   function getAll() {
-    return pokemonList;
+    return repository;
+  }
+
+  function addListPokemon(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listOfPokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-list');
+    listOfPokemon.appendChild(button);
+    pokemonList.appendChild(listOfPokemon);
   }
 
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListPokemon: addListPokemon
   };
 })();
 
+// New Pokemon added to the Repository
+pokemonRepository.add ({ name: 'Picachu', height: 0.4, types: ['electric']});
+pokemonRepository.add ({ name: 'Raichu', height: 0.8, types: ['electric']});
+
+console.log(pokemonRepository.getAll());
+
+// This function write a message how big is the pokemon.
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListPokemon(pokemon);
+});
+
+
+//
+// let pokemonList = document.querySelector('#pokemon-list');
+// let listItem = document.createElement('li');
+// listItem.innerText = '';
+// pokemonList.appendChild(listItem);
+//
+//
+// let listItem = document.querySelector('#pokemon-list > *');
+// let buttonList = document.createElement('button');
+// buttonList.innerText = pokemonRepository.name;
+// listItem.appendChild(buttonList);
+//
+// element.classList.add('button-list');
+
+
+
+function SizePokemonList (item) {
+  let bigHeight = ' - Wow, that\'s big!';
+  if (item.height > 1.7) {
+    document.write("<p>" + item.name + " (Height: " + item.height + " metre.) " + " Type: " + item.types + " " + bigHeight + "</p>");
+  } else {
+    document.write("<p>" + item.name + " (Height: " + item.height + " metre.) " + " Type: " + item.types + " " + "</p>");
+  }
+
+};
+
+pokemonRepository.getAll().forEach(SizePokemonList);
+
+
+
+
+
+/*
 // This function write a message how big is the pokemon.
 function SizePokemonList (item) {
   let bigHeight = ' - Wow, that\'s big!';
@@ -49,6 +106,4 @@ function SizePokemonList (item) {
 pokemonRepository.add ({ name: 'Picachu', height: 0.4, types: ['electric']});
 pokemonRepository.add ({ name: 'Raichu', height: 0.8, types: ['electric']});
 pokemonRepository.getAll().forEach(SizePokemonList);
-
-
-//************** 1 - Why If I use the "NEXT FUNCTION" doesn't print the pokemonList and appear instead ""[object,objcet], [object,objcet] (once for each bject of the array) ..."" in the screen? FUNCTION : document.write (pokemonRepository.getAll());***********
+*/
